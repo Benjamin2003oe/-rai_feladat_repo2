566cae4ee9ec31d72655cbc3e00d5a170455cb21
Benjamin2003OE <kvbenjamin@stud.uni-obuda.hu>
2023-10-25 14:39:04 +0200

    added for loop to the project

--- Órai_feladat_git/Program.cs
+++ Órai_feladat_git/Program.cs
@@ -8,6 +8,11 @@ namespace Órai_feladat_git
         {
             Console.Write("Type in a number: ");
             int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
         }
     }
 }
