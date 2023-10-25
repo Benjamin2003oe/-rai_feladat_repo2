1e1b7d0c6520df06fc146d4ed3b4937e6921e56d
Benjamin2003OE <kvbenjamin@stud.uni-obuda.hu>
2023-10-25 14:35:14 +0200

    added README

---
+++ Órai_feladat_git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Órai_feladat_git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
